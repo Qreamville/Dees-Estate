e4aa983786d0b0ad56c65fff9358a7c81d67948f
Qreamville <Oyeleyebalikis@yahoo.com>
2022-05-23 03:14:55 -1200

    Create navbar.jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 function App() {
   return (
-    <div className="App">
+    <div className="App flex justify-center align-middle container">
+      <p className="text-blue-900 text-3xl">ppppppp</p>
     </div>
   );
 }
