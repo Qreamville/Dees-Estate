add7d64f89f2cac5435842b21a324cd0a1ababa5
Qreamville <Oyeleyebalikis@yahoo.com>
2022-05-25 01:46:17 -1200

    Modify breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   theme: {
     screens: {
       xs: "320px",
-      sm: "480px",
-      md: "762px",
+      sm: "576px",
+      md: "768px",
       lg: "1024px",
       xl: "1200px",
       "2xl": "1920px",
